e068ef13714f914ea07e07d250ea7965f523740c
saikrishnakotari123 <71275733+saikrishnakotari123@users.noreply.github.com>
2020-09-22 18:30:23 +0530

    Add files via upload

---
+++ APIGateway/Controllers/HttpClientController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace APIGateway.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HttpClientController : ControllerBase
+    {
+        IConfiguration _iconfiguration;
+        public HttpClientController(IConfiguration iconfiguration)
+        {
+            _iconfiguration = iconfiguration;
+        }
+        [HttpGet]
+        public dynamic Get(string service , int param )
+        {
+            try
+            {
+                var ClientURL = string.Empty;
+                string queryString = string.Empty;
+                using (var client = new HttpClient())
+                {
+                    
+                    if(service =="User")
+                    {
+                       ClientURL = _iconfiguration["UserService:GetAllUser"];
+                      
+                    }      
+                    //client.DefaultRequestHeaders.Accept.Clear();
+                    //client.DefaultRequestHeaders.Add("REFSSessionID", refSessionId);
+                    client.DefaultRequestHeaders.Accept.Add(
+                        new MediaTypeWithQualityHeaderValue("application/json"));
+                    client.BaseAddress = new Uri(_iconfiguration["ServerUrl"]);
+                    var response = client.GetAsync(ClientURL).Result;
+
+                    var result = response.Content.ReadAsStringAsync().Result;
+                    dynamic dynamicObject = JsonConvert.DeserializeObject<dynamic>(result);
+                    return dynamicObject;
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
